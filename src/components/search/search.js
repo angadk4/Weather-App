8bd8a6a26fd5331c98c700aa098d8e4e8a5ef229
angadk4 <kheraangad@gmail.com>
2024-01-01 17:50:12 -0500

    Created basic search component

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,15 +1,24 @@
 import { AsyncPaginate } from "react-select-async-paginate";
 import { useState } from "react";
 
-const Search = () => {
+const Search = ({onSearchChange}) => {
 
     const[search, setSearch] = useState(null);
+
+    const handleOnChange = (searchData) => {
+        setSearch(searchData);
+        onSearchChange(searchData);
+    }
+
+    const loadOptions = (inputValue) => { }
 
     return (
         <AsyncPaginate
             placeholder="Search for city"
             debounceTimeout={600}
             value={search}
+            onChange={handleOnChange}
+            loadOptions={loadOptions}
         />
     )
 }
