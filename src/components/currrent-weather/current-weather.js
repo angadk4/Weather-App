c068dae3a6820b2ce65ef46e08ba7bed11402837
angadk4 <kheraangad@gmail.com>
2024-01-03 21:16:08 -0500

    Improvements and changes

--- src/components/currrent-weather/current-weather.js
+++ src/components/currrent-weather/current-weather.js
@@ -1,36 +1,61 @@
 import "./current-weather.css"
 
-const CurrentWeather = ({data}) => {
+const CurrentWeather = ({ data }) => {
     return (
         <div className="weather">
             <div className="top">
-                <div>
+                <div className="heading">
                     <p className="city">{data.city}</p>
-                    <p classname="weather-description">{data.weather[0].description}</p>
+                    <p className="weather-description">{data.weather[0].description}</p>
                 </div>
+                <p className="temperature">{Math.round(data.main.temp)}°C</p>
                 <img alt="weather" className="weather-icon" src={`icons/${data.weather[0].icon}.png`} />
             </div>
             <div className="bottom">
-                <p className="temperature">{Math.round(data.main.temp)}°C</p>
-                <div className="details">
-                    <div className="parameter-row">
-                        <span className="parameter-label top">Details</span>
+                <div className="bottomleft">
+                    <div className="details">
+                        <div className="parameter-row">
+                            <span className="parameter-label top">Current Conditions</span>
+                        </div>
+                        <div className="parameter-row">
+                            <span className="parameter-label">Feels like</span>
+                            <span className="parameter-value">{parseFloat(data.main.feels_like.toFixed(1))}°C</span>
+                        </div>
+                        <div className="parameter-row">
+                            <span className="parameter-label">Min Temp</span>
+                            <span className="parameter-value">{parseFloat(data.main.temp_min.toFixed(1))}°C</span>
+                        </div>
+                        <div className="parameter-row">
+                            <span className="parameter-label">Max Temp</span>
+                            <span className="parameter-value">{parseFloat(data.main.temp_max.toFixed(1))}°C</span>
+                        </div>
+                        <div className="parameter-row">
+                            <span className="parameter-label">Pressure</span>
+                            <span className="parameter-value">{Math.round(data.main.pressure)} hPa</span>
+                        </div>
                     </div>
-                    <div className="parameter-row">
-                        <span className="parameter-label">Feels like</span>
-                        <span className="parameter-value">{Math.round(data.main.feels_like)}°C</span>
-                    </div>
-                    <div className="parameter-row">
-                        <span className="parameter-label">Wind</span>
-                        <span className="parameter-value">{Math.round(data.wind.speed)} m/s</span>
-                    </div>
-                    <div className="parameter-row">
-                        <span className="parameter-label">Humidity</span>
-                        <span className="parameter-value">{Math.round(data.main.humidity)}%</span>
-                    </div>
-                    <div className="parameter-row">
-                        <span className="parameter-label">Pressure</span>
-                        <span className="parameter-value">{Math.round(data.main.pressure)} hPa</span>
+                </div>
+                <div className="bottomright">
+                    <div className="details">
+                        <div className="parameter-row">
+                            <span className="parameter-label top">Percipitation &amp; Sky</span>
+                        </div>
+                        <div className="parameter-row">
+                            <span className="parameter-label">Cloud Cover</span>
+                            <span className="parameter-value">{data.clouds.all}%</span>
+                        </div>
+                        <div className="parameter-row">
+                            <span className="parameter-label">Wind</span>
+                            <span className="parameter-value">{parseFloat(data.wind.speed.toFixed(1))} m/s</span>
+                        </div>
+                        <div className="parameter-row">
+                            <span className="parameter-label">Humidity</span>
+                            <span className="parameter-value">{Math.round(data.main.humidity)}%</span>
+                        </div>
+                        <div className="parameter-row">
+                            <span className="parameter-label">Visibility</span>
+                            <span className="parameter-value">{data.visibility}m</span>
+                        </div>
                     </div>
                 </div>
             </div>
--- src/components/forecast/forecast.js
+++ src/components/forecast/forecast.js
@@ -1,27 +1,34 @@
 import { Accordion, AccordionItem, AccordionItemButton, AccordionItemHeading, AccordionItemPanel } from "react-accessible-accordion";
 import './forecast.css';
 
-const weekDays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+const weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
 const Forecast = ({ data }) => {
-    const dayInAWeek = new Date().getDay();
-    const forecastDay = weekDays.slice(dayInAWeek, weekDays.length).concat(
-        weekDays.slice(0, dayInAWeek)
-    );
+    const getFormattedDate = (idx) => {
+        const now = new Date();
+        now.setHours(now.getHours() + 3 * (idx + 1)); // Start at now + 3 hours for the first entry
+        const day = weekDays[now.getDay()];
+        const time = now.toLocaleTimeString([], { hour: '2-digit', hour12: false }); // Only hour in 24-hour format
+        return `${day}, ${time}:00`;
+    };
 
     return (
         <>
-            <label className="title">Daily</label>
-            <Accordion allowZeroExpanded>
-                {data.list.slice(0, 7).map((item, idx) => (
+            <label className="title">3-hourly</label>
+            <Accordion allowZeroExpanded className="acc">
+                {data.list.slice(0, 16).map((item, idx) => (
                     <AccordionItem key={idx}>
                         <AccordionItemHeading>
                             <AccordionItemButton>
                                 <div className="daily-item">
+                                    <label className="arrow">▼</label>
                                     <img alt="weather" className="icon-small" src={`icons/${item.weather[0].icon}.png`} />
-                                    <label className="day">{forecastDay[idx]}</label>
-                                    <label className="description">{item.weather[0].description}</label>
-                                    <label className="min-max">{Math.round(item.main.temp_min)}°C / {Math.round(item.main.temp_max)}°C</label>
+                                    <label className="day">{getFormattedDate(idx)}</label>
+                                    <div className="right-group">
+                                        <label className="description">{item.weather[0].description}</label>
+                                        <label className="temp">{Math.round(item.main.temp)}°C</label>
+                                        <label className="feels-like">Feels Like: {Math.round(item.main.feels_like)}°C</label>
+                                    </div>
                                 </div>
                             </AccordionItemButton>
                         </AccordionItemHeading>
